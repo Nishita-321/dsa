31d7324b088b8c793750f5147816183d4d701faf
Nishita <nishitagarg027@gmail.com>
2026-07-30 16:55:35 +0530

    dsa questions

---
+++ 146/Solution.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+class LRUCache {
+
+    class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private HashMap<Integer, Node> map;
+    private int capacity;
+    private Node head, tail;
+
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insert(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key))
+            return -1;
+
+        Node node = map.get(key);
+
+        remove(node);
+        insert(node);
+
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(node);
+            map.remove(key);
+        }
+
+        Node newNode = new Node(key, value);
+
+        insert(newNode);
+        map.put(key, newNode);
+
+        if (map.size() > capacity) {
+
+            Node lru = tail.prev;
+
+            remove(lru);
+            map.remove(lru.key);
+        }
+    }
+}
+
